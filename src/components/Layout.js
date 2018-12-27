4c2e1fc8342d9c1999e5a3e8455805ea6ff510e6
devolasvegas <devondaviau@yahoo.ca>
2018-12-27 16:20:41 -0500

    add ThemeProvider and GlobalStyle components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from "gatsby"
-import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import Navbar from '../components/Navbar'
 import './all.sass'
+
+// Gel site theme settings
+const theme = {
+  darkGrey: '#434244',
+}
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'futura';
+  }
+`;
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
@@ -26,17 +37,18 @@ const TemplateWrapper = ({ children }) => (
           <meta name="description" content={data.site.siteMetadata.description} />
           
           <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-	
-	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
-	        <meta name="theme-color" content="#fff" />
+          <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+  
+          <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
+          <meta name="theme-color" content="#fff" />
 
-	        <meta property="og:type" content="business.business" />
+          <meta property="og:type" content="business.business" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/og-image.jpg" />
         </Helmet>
+        <GlobalStyle />
         <Navbar />
         <div>{children}</div>
       </div>
@@ -44,4 +56,10 @@ const TemplateWrapper = ({ children }) => (
   />
 )
 
-export default TemplateWrapper
+const WrappedWithThemeProvider = ({ children }) => (
+  <ThemeProvider theme={ theme }>
+    <TemplateWrapper children={children} />
+  </ThemeProvider>
+);
+
+export default WrappedWithThemeProvider
