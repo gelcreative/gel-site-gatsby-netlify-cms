955e718451c22f0f01deb12bbeed1e7b77915a2a
devolasvegas <devondaviau@yahoo.ca>
2018-12-28 15:04:48 -0500

    build second footer column, add some global styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,10 @@ import styled from 'styled-components'
 
 import logo from '../img/logo.svg'
 
+const StyledFooter = styled.footer`
+  margin-bottom: 50px;
+`;
+
 const FooterTop = styled.div`
   display: flex;
   align-items: center;
@@ -12,21 +16,29 @@ const FooterTop = styled.div`
     justify-content: center;
   }
 `
-const StyledFooterInner = styled.footer`
+const StyledFooterInner = styled.div`
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
 `
 const FooterItemContainer = styled.div`
   margin: 1em;
+  min-width: 33%;
+  max-width: 300px;
+  flex-grow: 1;
 `
 
 const StyledAddress = styled.address`
-  color: ${ props => props.theme.darkGrey };
+  font-style: normal;
 `
 
+const StyledFooterNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
 const Footer = () => (
-  <footer>
+  <StyledFooter>
     <FooterTop>
       <FooterItemContainer>
         <Link to="/" className="navbar-item" title="Logo">
@@ -40,15 +52,31 @@ const Footer = () => (
           89 Collier Street, Suite 201 <br />
           Barrie, ON L4M 1H2
         </StyledAddress>
+        <div><a href="mailto:connect@gelmarketing.ca">connect@gelmarketing.ca</a></div>
+        <div><a href="tel:+17057277980">705.727.7980</a></div>
       </FooterItemContainer>
       <FooterItemContainer>
-
+        <StyledFooterNav>
+          <div>
+            <ul>
+              <li>home</li>
+              <li>portfolio</li>
+              <li>about</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>blog</li>
+              <li>contact</li>
+            </ul>
+          </div>
+        </StyledFooterNav>
       </FooterItemContainer>
       <FooterItemContainer>
 
       </FooterItemContainer>
     </StyledFooterInner>
-  </footer>
+  </StyledFooter>
 )
 
 export default Footer;
