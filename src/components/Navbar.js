19069135e687573db07ec5c12acf1e0f2918c5ac
Gel Studio <devondaviau@yahoo.ca>
2019-01-09 13:41:51 -0500

    a bit of JS rejigging and navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,11 +17,15 @@ const Navbar = class extends React.Component {
  
          // Get the target from the "data-target" attribute
          const target = el.dataset.target;
+         // Need to select the navbar too, for some Gel styles
+         const navBar = document.querySelector('.navbar');
          const $target = document.getElementById(target);
  
          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
          el.classList.toggle('is-active');
          $target.classList.toggle('is-active');
+         // Toggle this class so we can select it and position it
+         navBar.classList.toggle('gel-nav-active');
  
        });
      });
@@ -46,7 +50,7 @@ const Navbar = class extends React.Component {
       </div>
       <div id="navMenu" className="navbar-menu">
         <div className="navbar-start has-text-centered">
-        <p>Learn <Link className="gel-navbar-item" to="/about">about</Link> Gel Marketing, take <br />a look at our portfolio, get in <br /><Link className="gel-navbar-item" to="/contact">contact</Link>, or read our blog.</p>
+        <p>Learn <Link className="gel-navbar-item" to="/about">about</Link> Gel Marketing, take a look at our portfolio, get in <Link className="gel-navbar-item" to="/contact">contact</Link>, or read our blog.</p>
         </div>
       </div>
     </div>
