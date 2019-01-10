ceb7b0f3012851f69e1611b4d81a83b4686a728c
Gel Studio <devondaviau@yahoo.ca>
2019-01-10 12:04:16 -0500

    adjust tabs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,32 +4,32 @@ import HeaderLogo from './svg/HeaderLogo'
 
 const Navbar = class extends React.Component {
 
-  componentDidMount() {
+componentDidMount() {
     // Get all "navbar-burger" elements
-   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
     // Check if there are any navbar burgers
-   if ($navbarBurgers.length > 0) {
- 
-     // Add a click event on each of them
-     $navbarBurgers.forEach( el => {
-       el.addEventListener('click', () => {
- 
-         // Get the target from the "data-target" attribute
-         const target = el.dataset.target;
-         // Need to select the navbar too, for some Gel styles
-         const navBar = document.querySelector('.navbar');
-         const $target = document.getElementById(target);
- 
-         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-         el.classList.toggle('is-active');
-         $target.classList.toggle('is-active');
-         // Toggle this class so we can select it and position it
-         navBar.classList.toggle('gel-nav-active');
- 
-       });
-     });
-   }
- }
+    if ($navbarBurgers.length > 0) {
+
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          // Need to select the navbar too, for some Gel styles
+          const navBar = document.querySelector('.navbar');
+          const $target = document.getElementById(target);
+
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+          // Toggle this class so we can select it and position it
+          navBar.classList.toggle('gel-nav-active');
+
+        });
+      });
+    }
+  }
  
  render() {
    return (
