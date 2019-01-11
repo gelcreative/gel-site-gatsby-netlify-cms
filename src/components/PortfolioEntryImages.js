76ca02c973d6ac04992785fd84922946c4883b25
devolasvegas <devondaviau@yahoo.ca>
2019-01-11 18:10:05 -0500

    create portfolio images component

---
+++ src/components/PortfolioEntryImages.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import PreviewCompatibleImage from './PreviewCompatibleImage'
+
+const PortfolioEntryImages = ({portfolioImages}) => {
+  return (
+    portfolioImages.map(image => (
+      <div className="gel-project-image-inner">
+        <PreviewCompatibleImage imageInfo={image}/>
+      </div>
+    ))
+  )
+}
+
+PortfolioEntryImages.propTypes = {
+  portfolioImages: PropTypes.arrayOf(
+    PropTypes.shape({
+      image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    }),
+  ),
+}
+
+export default PortfolioEntryImages;
