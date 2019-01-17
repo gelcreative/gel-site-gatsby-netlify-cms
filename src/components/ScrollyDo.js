237e6eb85422ba31bcbd922e984fc5ef16794f1e
Gel Studio <devondaviau@yahoo.ca>
2019-01-17 11:01:36 -0500

    bring in gif and create scroll indicator section component

---
+++ src/components/ScrollyDo.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import SocialIcons from './SocialIcons'
+
+const ScollyDoContainer = styled.div``
+
+class ScrollyDo extends React {
+  componentDidMount() {
+
+  }
+  render ({icons}) {
+    return (
+      <ScrollyDoContainer>
+
+      </ScrollyDoContainer>
+    )
+  }
+}
+
+export default ScrollyDo
