63bddc5a5f8d254402c888c30cb3f6c1d0f96c19
Gel Studio <devondaviau@yahoo.ca>
2019-01-17 10:22:52 -0500

    move footer logo styles into styled component

--- src/components/svg/FooterLogo.js
+++ src/components/svg/FooterLogo.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import Icon from '-!svg-react-loader?name=GelLogo!../../img/gel-logo-footer.svg'
+
+const FooterLogoContainer = styled.div`
+  .gel-footer-logo {
+    width: 100px;
+  }
+
+  .gel-footer-logo:hover circle {
+    animation: gelHoverColors;
+    animation-duration: 2s;
+    animation-fill-mode: forwards;
+  }
+
+  .gel-footer-logo:hover .gel-logo-letter {
+    animation: gelLetterScale;
+    animation-duration: 2s;
+    animation-fill-mode: forwards;
+    transform-origin: center;
+  }
+`
 
 export default class FooterLogo extends Component {
   render() {
-    return <Icon className="gel-footer-logo" />
+    return (
+      <FooterLogoContainer>
+        <Icon className="gel-footer-logo" />
+      </FooterLogoContainer>
+    )
   }
 }
