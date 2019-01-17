95030eb24d34db8b202d0ab07274a15743bd010b
Gel Studio <devondaviau@yahoo.ca>
2019-01-17 10:07:29 -0500

    move animation call into a styled component

--- src/components/svg/HeaderLogo.js
+++ src/components/svg/HeaderLogo.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import Icon from '-!svg-react-loader?name=GelLogo!../../img/gel-logo-header.svg'
 
+const HeaderLogoContainer = styled.div`
+  .gel-header-logo:hover #Grey_-fixed_-_heavy path,
+  .gel-header-logo:hover #Grey_-fixed_-_heavy polygon {
+    animation: gelHoverColors;
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+  }
+`
 export default class HeaderLogo extends Component {
   render() {
-    return <Icon className="gel-header-logo" />
+    return (
+      <HeaderLogoContainer>
+        <Icon className="gel-header-logo" />
+      </HeaderLogoContainer>
+    )
   }
 }
