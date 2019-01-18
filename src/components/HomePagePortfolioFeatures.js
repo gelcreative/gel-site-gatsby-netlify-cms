55a3728a0c2a82d22959c32054669dffef9432b1
Gel Studio <devondaviau@yahoo.ca>
2019-01-18 11:54:30 -0500

    pass in new data and start styling

--- src/components/HomePagePortfolioFeatures.js
+++ src/components/HomePagePortfolioFeatures.js
@@ -4,6 +4,27 @@ import { kebabCase } from 'lodash'
 import styled from 'styled-components'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 
+const StyledFeaturesSection = styled.section`
+    position: relative;
+
+  .gel-homepage-featured-text-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      h2 {
+      color: #ffffff;
+      font-size: 3.5rem;
+    }
+  }
+
+`
+
 const HomePagePortfolioFeatures = () => (
   <StaticQuery
     query={graphql`
@@ -13,6 +34,8 @@ const HomePagePortfolioFeatures = () => (
             node {
               id
               frontmatter {
+                title
+                project_type
                 home_page_featured {
                   featured_image {
                     image {
@@ -36,11 +59,15 @@ const HomePagePortfolioFeatures = () => (
       return (
         edges.map(edge => {
           return (
-            <section key={edge.node.id} className="gel-homepage-featured columns">
-              <div className="column">  
+            <StyledFeaturesSection key={edge.node.id} className="gel-homepage-featured columns">
+              <div className="column">
                 <PreviewCompatibleImage imageInfo={edge.node.frontmatter.home_page_featured.featured_image} />
+                <div className="gel-homepage-featured-text-container">
+                  <h2>{edge.node.frontmatter.project_type}</h2>
+                  <Link className="button gel-button-1 is-large is-dark" to={`/portfolio-entries/${kebabCase(edge.node.frontmatter.title)}`}>View Case Study</Link>
+                </div>
               </div>
-            </section>
+            </StyledFeaturesSection>
           )
         })
       )
