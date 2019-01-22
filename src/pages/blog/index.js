350fd55ddfb3f5d2e2281d44f187e86de627ed77
Gel Studio <devondaviau@yahoo.ca>
2019-01-22 15:01:43 -0500

    change some things

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import styled from 'styled-components'
 import Layout from '../../components/Layout'
 
+const BlogSection = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+`
+
 const BlogPage = ({data}) => {
+  
   const { edges: posts } = data.allMarkdownRemark
   
   return (
     <Layout>
       <div className="container">
-        <section className="columns gel-blog-container-outer">
+        <BlogSection className="columns gel-blog-container-outer">
           <h1 className="visually-hidden">Gel's Blog</h1>
             {posts.map((edge, i) => {
+              let columnClass
+              if((i + 1) < 4 && (i + 1) % 2 === 0) {
+                columnClass = " is-one-third"
+              } else if((i + 1) % 3 === 0) {
+                columnClass = " is-two-thirds"
+              } else if((i + 1) % 4 === 0) {
+                columnClass = " is-two-thirds"
+              }  else if((i + 1) % 5 === 0) {
+                columnClass = " is-one-third"
+              } else {
+                columnClass = " is-full"
+              }
               return (
-                <div className={"column " + toString((i%2 == 0 && "dookie"))} key={edge.node.id}>{edge.node.frontmatter.title}</div>
+                <div className={"column" + columnClass } key={edge.node.id}>{edge.node.frontmatter.title}</div>
               )
             })}
-        </section>
+        </BlogSection>
       </div>
     </Layout>
   )
