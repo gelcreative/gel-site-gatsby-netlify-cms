bc5c2d7435d052597c7e8cc2c96bd34d0d8b3eff
Gel Studio <devondaviau@yahoo.ca>
2019-01-23 16:49:59 -0500

    text styles, import styled-components

--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
+import styled from 'styled-components'
 import Layout from '../components/Layout'
 import PortfolioGrid from '../components/PortfolioGrid'
+
+const StyledPortfolioPage = styled.article`
+  &.container {
+    margin-top: 50px;
+  }
+  .gel-portfolio-intro {
+    font-size: 2rem;
+  }
+`
 
 export const PortfolioPageTemplate = ({
   title,
   portfolioIntro
 }) => {
   return (
-      <div className="container">
-        <section className="section">
-          <h1 className="visually-hidden">{title}</h1>
-          <p>{portfolioIntro}</p>
+      <StyledPortfolioPage className="container">
+        <section className="section columns is-centered">
+          <div className="column has-text-centered is-10 is-offset-1">
+            <h1 className="visually-hidden">{title}</h1>
+            <p className="gel-portfolio-intro">{portfolioIntro}</p>
+          </div>
         </section>
         <section className="section">
           <PortfolioGrid />
         </section>
-      </div>
+      </StyledPortfolioPage>
   )
 }
 
