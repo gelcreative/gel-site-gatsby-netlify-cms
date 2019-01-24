593655ec4a75d07c43d284d787981115cf5d3581
Gel Studio <devondaviau@yahoo.ca>
2019-01-24 11:14:22 -0500

    rejig the portfolio entry template

--- src/templates/portfolio-entry.js
+++ src/templates/portfolio-entry.js
@@ -8,47 +8,10 @@ import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 import PortfolioEntryImages from '../components/PortfolioEntryImages'
 
 const StyledPortfolioEntry = styled.div`
-  max-width: 960px;
-  width: 90%;
-  margin: 0 auto;
-  .gel-project-image-inner + .gel-project-image-inner {
-    margin-top: 5px;
-  }
-
-  .gel-portfolio-entry-main-text {
-    display: flex;
-    align-items: center;
-  }
-
-  .gel-portfolio-entry-masthead .column {
-    height: 300px;
-  }
-
-  .gel-portfolio-entry-masthead .gatsby-image-wrapper {
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-  }
-
-  .gel-portfolio-entry-masthead .gatsby-image-wrapper * {
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    margin: 0 auto;
-  }
-
-  .gel-portfolio-entry-masthead .gatsby-image-wrapper > div {
-    display: none;
-  }
-
-  .gel-portfolio-entry-masthead .gatsby-image-wrapper img {
-    width: auto !important;
-    max-height: 300px !important;
-  }
+    .gel-portfolio-entry-masthead {
+      max-width: 600px;
+      margin: 0 auto;
+    }
 `
 
 export const PortfolioEntryTemplate = ({
@@ -149,7 +112,7 @@ export const pageQuery = graphql`
       header_image {
         image {
           childImageSharp {
-            fluid (maxHeight: 300, quality: 100) {
+            fluid (maxWidth: 500, quality: 100) {
               ...GatsbyImageSharpFluid
             }
           }
