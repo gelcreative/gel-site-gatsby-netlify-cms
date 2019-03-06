b14ef7695a76010a2b1b732feb08876f4d71efda
Gel Studio <devondaviau@yahoo.ca>
2019-03-06 14:45:12 -0500

    rejig the layout to have the masonry layout from the blog

--- src/components/PortfolioGrid.js
+++ src/components/PortfolioGrid.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import { kebabCase } from 'lodash'
 import styled from 'styled-components'
-import PreviewCompatibleImage from './PreviewCompatibleImage';
 
 const StyledPortfolioGrid = styled.section`
   .gel-portfolio-grid {
@@ -13,7 +12,59 @@ const StyledPortfolioGrid = styled.section`
     position: relative;
   }
 
-  .gel-portfolio-grid-item .gatsby-image-wrapper:nth-of-type(2) {
+  .column:first-of-type {
+    height: 640px;
+  }
+
+  .column {
+    height: 420px;
+    justify-content: stretch;
+  }
+
+  .gel-portfolio-item-inner {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding: 30px 3%;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    transition: 300ms;
+    overflow: hidden;
+  }
+
+  .gel-portfolio-item-image {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    padding: 30px 3%;
+    background: center no-repeat;
+    background-size: cover;
+  }
+
+  .column:nth-child(odd) .gel-portfolio-item-image {
+    background-color: ${props => props.theme.typeGrey};
+  }
+
+  .column:nth-child(odd) .gel-portfolio-item-image:hover {
+    background-color: ${props => props.theme.blue};
+  }
+
+  .column:nth-child(even) .gel-portfolio-item-image {
+    background-color: ${props => props.theme.lightGrey};
+  }
+
+  .column:nth-child(even) .gel-portfolio-item-image:hover {
+    background-color: ${props => props.theme.orange};
+  }
+
+  .gel-portfolio-grid-gel-portfolio-item-image:nth-of-type(2) {
       position: absolute !important;
       left: 0;
       right: 0;
@@ -60,22 +111,16 @@ const StyledPortfolioGrid = styled.section`
     width: 80%;
   }
 
-  @media (min-width: 769px) {
-    .gel-portfolio-grid-item:hover .gatsby-image-wrapper:nth-of-type(2) {
-        opacity: 0;
-    }
+  .gel-portfolio-grid-item .gel-portfolio-item-colour-image {
+      opacity: 1;
   }
 
-  @media (max-width: 768px) {
-    .gel-portfolio-grid-item .gatsby-image-wrapper:nth-of-type(2) {
-        opacity: 1;
-    }
-    .gel-portfolio-grid-item .gel-portfolio-item-text-container {
-      width: 90%;
-      height: 90%;
-      border-radius: 50%;
-      opacity: 1;
-    }
+  .gel-portfolio-grid-item:hover .gel-portfolio-item-colour-image {
+      opacity: 0;
+  }
+
+  .gel-portfolio-grid-item:hover .gel-portfolio-item-text-container {
+    transform: scale(1.1);
   }
 `
 
@@ -133,19 +178,70 @@ const PortfolioGrid = () => (
       const { edges: portfolioEntries } = data.mainPortfolioQuery
       const { edges: grayscaleGridImages } = data.grayscaleGridImageQuery
       const defaults = data.allFile.edges
+
+      let columnClass = ""
+      let columnCounter = 1
+
       return (
         <StyledPortfolioGrid>
-          <div className="gel-portfolio-grid columns is-gapless">
+          <div className="gel-portfolio-grid columns">
             {portfolioEntries.map((portfolioEntry, i) => {
+
+              if(columnCounter > 5) {
+                columnCounter = 1
+              }
+
+              switch(columnCounter) {
+                case 1:
+                  columnClass = " is-full"
+                  break;
+                case 2:
+                  columnClass = " is-one-third"
+                  break;
+                case 3:
+                  columnClass = " is-two-thirds"
+                  break;
+                case 4:
+                  columnClass = " is-two-thirds"
+                  break;
+                case 5:
+                  columnClass = " is-one-third"
+                  break;
+                default:
+                  columnClass = " is-full"
+              }
+
+              columnCounter++
+
               return (
-                <div key={portfolioEntry.node.id} className="column is-half gel-portfolio-grid-item">
-                  <PreviewCompatibleImage imageInfo={(grayscaleGridImages[i].node.frontmatter.colour_grid_image ? grayscaleGridImages[i].node.frontmatter.colour_grid_image : defaults[0].node)} />
-                  <PreviewCompatibleImage imageInfo={(portfolioEntry.node.frontmatter.colour_grid_image ? portfolioEntry.node.frontmatter.colour_grid_image : defaults[1].node)} />
-                  <Link to={`/portfolio-entries/${kebabCase(portfolioEntry.node.frontmatter.title)}`}>
-                    <div className="gel-portfolio-item-text-container">
-                      <p>{portfolioEntry.node.frontmatter.project_type} for <br /><span>{portfolioEntry.node.frontmatter.title}</span></p>
-                    </div>
-                  </Link>
+                <div key={portfolioEntry.node.id} className={`column gel-portfolio-grid-item${columnClass}`}>
+                  <div className="gel-portfolio-item-inner">
+                    <div 
+                      className="gel-portfolio-item-grayscale-image gel-portfolio-item-image" 
+                      style={{
+                        backgroundImage: `url(${
+                          grayscaleGridImages[i].node.frontmatter.colour_grid_image.childImageSharp
+                            ? grayscaleGridImages[i].node.frontmatter.colour_grid_image.childImageSharp.fluid.src
+                            : defaults[0].node
+                        })`,
+                    }}></div>
+                    <div 
+                      className="gel-portfolio-item-colour-image gel-portfolio-item-image" 
+                      style={{
+                        backgroundImage: `url(${
+                          portfolioEntry.node.frontmatter.colour_grid_image.childImageSharp
+                            ? portfolioEntry.node.frontmatter.colour_grid_image.childImageSharp.fluid.src
+                            : defaults[1].node
+                        })`,
+                    }}></div>
+                    {/* <PreviewCompatibleImage imageInfo={(grayscaleGridImages[i].node.frontmatter.colour_grid_image ? grayscaleGridImages[i].node.frontmatter.colour_grid_image : defaults[0].node)} />
+                    <PreviewCompatibleImage imageInfo={(portfolioEntry.node.frontmatter.colour_grid_image ? portfolioEntry.node.frontmatter.colour_grid_image : defaults[1].node)} /> */}
+                    <Link to={`/portfolio-entries/${kebabCase(portfolioEntry.node.frontmatter.title)}`}>
+                      <div className="gel-portfolio-item-text-container">
+                        <p>{portfolioEntry.node.frontmatter.project_type} for <br /><span>{portfolioEntry.node.frontmatter.title}</span></p>
+                      </div>
+                    </Link>
+                  </div>
                 </div>
               )
             })}
