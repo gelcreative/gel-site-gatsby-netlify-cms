124a76e0951e4435796f23571d816abdf4fbeac8
Gel Studio <devondaviau@yahoo.ca>
2019-03-07 12:21:33 -0500

    register new template with CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,11 @@
 import CMS from 'netlify-cms'
 
+import IndexPagePreview from './preview-templates/IndexPagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import PortfolioEntryPreview from './preview-templates/PortfolioEntryPreview'
 
+CMS.registerPreviewTemplate('index-page', IndexPagePreview)
 CMS.registerPreviewTemplate('about-page', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('portfolio-entry', PortfolioEntryPreview)
