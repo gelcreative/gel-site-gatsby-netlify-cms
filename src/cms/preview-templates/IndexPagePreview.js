5652d25a912eb17f9ef6ae3138d84b547f11a857
Gel Studio <devondaviau@yahoo.ca>
2019-03-07 12:20:25 -0500

    create home page cms preview template

---
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { IndexPageTemplate } from '../../templates/index-page'
+
+const IndexPagePreview = ({ entry }) => {
+  return (
+    <IndexPageTemplate
+      title={entry.getIn('data','title')}
+      services={entry.getIn('data','services')}
+      intro={entry.getIn('data','intro')}
+      featuredPortfolioTitle={entry.getIn('data','featuredPortfolioTitle')}
+      clientListTitle={entry.getIn('data','clientListTitle')}
+    />
+  )
+}
+
+IndexPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  })
+}
+
+export default IndexPagePreview
