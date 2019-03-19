52639a3d054864c4b0f02b27ec47fdd2a8e60ad5
Gel Studio <devondaviau@yahoo.ca>
2019-03-19 12:35:06 -0400

    fix broken import

--- src/components/HomePagePortfolioFeatures.js
+++ src/components/HomePagePortfolioFeatures.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, StaticQuery, graphql } from 'gatsby'i
+import { Link, StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
 
