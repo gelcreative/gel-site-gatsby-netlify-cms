a2caded11c705dcb9f17e182369bd562f307de7c
Ryan Amalfitano <ryan.amalfitano@gmail.com>
2020-05-11 17:24:43 -0400

    Updated popup styling
    Made the popup have a maximum height and center vertically on larger screens. Also added delay to popup

--- src/html.js
+++ src/html.js
@@ -25,15 +25,17 @@ export default function HTML(props) {
                   __html: `
                         // COVID-19 Popup
 
-                        window.onload = function () {
+                        window.onload = function () { setTimeout(createPopup, 5000); }
+
+                        function createPopup() {
                             // Grab cookies.
                             var cookies = decodeURIComponent(document.cookie);
 
                             // If we can't find the popup cookie, create and show the popup.
                             if (cookies.indexOf("c19popup") == -1) {
                                 var popupEl = document.createElement("div");
-                                popupEl.innerHTML = "<img src='/img/2020_gel_popup-01-01.jpg' alt='Gel is Open' style='max-height: 100%; margin: 0 auto;' />";
-                                popupEl.style = "position: fixed; top: 0; width: 100%; height: 100%; z-index: 999; display: grid; justify-content: center; background: rgba(0, 0, 0, 0.35);";
+                                popupEl.innerHTML = "<img src='/img/2020_gel_popup-01-01.jpg' alt='Gel is Open' style='height:739px; max-height:100%; margin:0 auto;' />";
+                                popupEl.style = "position:fixed; top:0; width:100%; height:100%; z-index:999; display:grid; justify-content:center; align-content:center; background:rgba(0, 0, 0, 0.35);";
 
                                 document.body.appendChild(popupEl);
                             }
