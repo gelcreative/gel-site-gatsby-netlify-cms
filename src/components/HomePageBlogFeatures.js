1e4ca92d6c4dd79ab47fb3be7d253d92ca6419e5
Ryan Amalfitano <ryan.amalfitano@gmail.com>
2020-11-08 13:58:09 -0500

    Update HomePageBlogFeatures.js
    - Fixed some of the spacing on the features
    - Moved buttons to anchor at the bottom of each item, and line up with the others
    - Hid date
    - Updated button text

--- src/components/HomePageBlogFeatures.js
+++ src/components/HomePageBlogFeatures.js
@@ -4,18 +4,20 @@ import styled from 'styled-components';
 import PreviewCompatibleImage from './PreviewCompatibleImage';
 
 const StyledPostsSection = styled.div`
+  position: relative;
   max-width: 530px;
   flex-basis: 32%;
 
-
   h2 {
+    margin: 10px 0 0;
     font-family: ${props => props.theme.secondaryFont};
-    font-weight: lighter;
-    font-size: 3.8rem;
+    font-weight: bold;
+    font-size: 3.6rem;
+    line-height: 4.5rem;
   }
 
   h6 {
-    margin: 0 0 30px;
+    margin: 10px 0 30px;
     font-family: ${props => props.theme.regularFont};
     font-weight: regular;
     font-size: 1.8rem;
@@ -23,10 +25,17 @@ const StyledPostsSection = styled.div`
   }
 
   p {
-    margin: 0 0 20px;
+    max-width: 65rem;
+    margin: 30px auto 0;
+    padding-bottom: 75px;
     font-family: ${props => props.theme.regularFont};
     font-weight: lighter;
     font-size: 1.8rem;
+  }
+
+  button {
+    position: absolute;
+    bottom: 1rem;
   }
 `;
 
@@ -70,22 +79,20 @@ const HomePageBlogFeatures = () => (
         return (
           <StyledPostsSection
             key={edge.node.id}
-            className="gel-homepage-post"
+            className="gel-homepage-post column"
           >
-            <div className="">
-              <PreviewCompatibleImage
-                imageInfo={
-                  edge.node.frontmatter.thumbnail_image
-                }
-                className="gel-homepage-featured-image"
-              />
+            <PreviewCompatibleImage
+              imageInfo={
+                edge.node.frontmatter.thumbnail_image
+              }
+              className="gel-homepage-featured-image"
+            />
 
-              <div className="gel-homepage-featured-text-container">
-                <h2>{edge.node.frontmatter.title}</h2>
-                <h6>{edge.node.frontmatter.date}</h6>
-                <p>{edge.node.frontmatter.description}</p>
-                <button className="button gel-button-2 is-primary"><Link to={edge.node.fields.slug}></Link>Read the Blog</button>
-              </div>
+            <div className="gel-homepage-featured-text-container">
+              <h2>{edge.node.frontmatter.title}</h2>
+            {/*<h6>{edge.node.frontmatter.date}</h6>*/}
+              <p>{edge.node.frontmatter.description}</p>
+              <button className="button gel-button-2 is-primary"><Link to={edge.node.fields.slug}></Link>Read More</button>
             </div>
           </StyledPostsSection>
         );
