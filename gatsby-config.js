a3219571e6d810ef6f128f0accf4d93123b1291d
Ryan Amalfitano <ryan.amalfitano@gmail.com>
2020-11-14 01:40:34 -0500

    Update gatsby-config.js
    Added file access to the filesystem.

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/static/files`,
+        name: 'files',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
         ignore: [`**/trash_bin/**`]
